476ae8f552c2cc004de642333daabe8a07218572
aaroexxt <aaron.becker@gmail.com>
2018-11-11 23:22:19 -0800

    oof

--- main.js
+++ main.js
@@ -23,10 +23,10 @@ const frontendFile = "main.html";
 const controllerA = new gamePad('ps4/dualshock4', {
 	productID: 2508
 });
-const controllerB = new gamePad('logitech/gamepadf310');
+const controllerB = new gamePad('ps4/dualshock4');
 
 //WHAT CONTROLLERS ARE ENABLED
-const controllerAEnabled = true;
+const controllerAEnabled = false;
 const controllerBEnabled = false;
 
 //CONTROLLER SETTINGS
@@ -102,17 +102,34 @@ var currentControllerPositions = {
 	}
 }
 
-var currentRobot = {
+var currentRobotA = {
 	x: 100,
 	y: 100,
 	angle: 0,
-	vel: 0,
-	accel: 0,
-	width: 10,
-	height: 10
+	velocityX: 0,
+	velocityY: 0,
+	width: 100,
+	height: 100,
+	moveFriction: 0.1,
+	angFriction: 0.1,
+	maxSpeed: 3,
+	omega: 0, //ang vel
+	endpoints: [[0,0], [10,0], [10,10], [0,10]]
 }
-
-var robotEndpoints = [[0,0], [10,0], [10,10], [0,10]];
+var currentRobotB = {
+	x: 100,
+	y: 100,
+	angle: 0,
+	velocityX: 0,
+	velocityY: 0,
+	width: 100,
+	height: 100,
+	moveFriction: 0.1,
+	angFriction: 0.1,
+	maxSpeed: 3,
+	omega: 0, //ang vel
+	endpoints: [[0,0], [10,0], [10,10], [0,10]]
+}
 
 const normalizeControls = function(options) {
 	let xDelta = options.x-options.stick.x;
@@ -121,40 +138,162 @@ const normalizeControls = function(options) {
 	}
 	options.position.x = xDelta;
 	let yDelta = options.y-options.stick.y;
-	yDelta*=-1; //idk why but y is inverted
+	//yDelta*=-1; //idk why but y is inverted
 	if (Math.abs(yDelta) < options.deadZone) {
 		yDelta = 0;
 	}
 	options.position.y = yDelta;
-
-	updateRobotPosition(options.position.x, options.position.y, options.position.x, options.position.y);
-	findRobotEndpoints();
 }
 
-const updateRobotPosition = function(powerLeftX, powerLeftY, powerRightX, powerRightY) {
-	//let x = currentRobotPosition.x;
-	//let y = currentRobotPosition.y;
-	//let a = currentRobotPosition.angle;
+const updateRobotPosition = function(robot, deltaTime, powerLeftX, powerLeftY, powerRightX, powerRightY) {
+	//ALL The functions return deltas
+	let strafeF = strafeRobot(powerLeftX, robot); //returns deltas
 
-	//let dP = powerLeft-powerRight;
-	currentRobot.x += powerLeftX*0.05;
-	currentRobot.y += powerLeftY*0.05;
+	let moveF = moveRobotFB(powerLeftY, robot);
+	let angle = turnRobot(powerRightX, robot);
 
+	robot.velocityX += strafeF[0]+moveF[0]*2;
+	robot.velocityY += strafeF[1]+moveF[1]*2;
+
+	if (robot.velocityX < 0) {
+		robot.velocityX += robot.moveFriction;
+	} else if (robot.velocityX !== 0) {
+		robot.velocityX -= robot.moveFriction;
+	}
+
+	if (robot.velocityY < 0) {
+		robot.velocityY += robot.moveFriction;
+	} else if (robot.velocityY !== 0) {
+		robot.velocityY -= robot.moveFriction;
+	}
+
+	if (robot.velocityX > robot.maxSpeed) {
+		robot.velocityX = robot.maxSpeed;
+	} else if (robot.velocityX < -robot.maxSpeed) {
+		robot.velocityX = robot.maxSpeed;
+	}
+	if (robot.velocityY > robot.maxSpeed) {
+		robot.velocityY = robot.maxSpeed;
+	} else if (robot.velocityY < -robot.maxSpeed) {
+		robot.velocityY = robot.maxSpeed;
+	}
+
+	robot.x += robot.velocityX*deltaTime;
+	robot.y += robot.velocityY*deltaTime;
+
+	robot.omega += angle;
+	if (robot.omega < 0) {
+		robot.omega +=robot.angFriction;
+	} else if (robot.omega !== 0) {
+		robot.omega -=robot.angFriction;
+	}
+
+	robot.angle += (robot.omega*(deltaTime/50));
+	robot.angle = constrainAngle(robot.angle);
+
+	//console.log(robot.velocityX, robot.velocityY, moveF)
+
+	//robot.angle = deltaTheta*deltaTime; //lol
+	//robot.omega = turnD;
+	//robot.
+}
+const reduceRobotFinal = (robot) => {
+	console.log("reduce")
+	if (robot.angle < 0.2) {
+		robot.angle = 0;
+	} else if (robot.angle > -0.2) {
+		robot.angle = 0;
+	}
+
+	if (robot.velocityY < 0.01) {
+		robot.velocityY = 0;
+	} else if (robot.velocityY > -0.01) {
+		robot.velocityY = 0;
+	}
+	if (robot.velocityX < 0.01) {
+		robot.velocityX = 0;
+	} else if (robot.velocityX > -0.01) {
+		robot.velocityX = 0;
+	}
+}
+const strafeRobot = function(power, robot) {
+	let strafeAngle = (robot.angle+90);
+	strafeAngle = constrainAngle(strafeAngle);
+
+	strafeDistance = fFromJoystickPower(power);
+	return [-strafeDistance*Math.cos(strafeAngle), -strafeDistance*Math.sin(strafeAngle)];
+}
+const moveRobotFB = function(power, robot) {
+	let moveAngle = robot.angle;
+	moveAngle = constrainAngle(moveAngle);
+
+	moveDistance = fFromJoystickPower(power);
+	return [moveDistance*Math.cos(moveAngle), moveDistance*Math.sin(moveAngle)];
+}
+const turnRobot = function(power, robot) {
+	let turnAngle = (degFromJoystickPower(power));
+	//turnAngle = constrainAngle(turnAngle);
+	//turnAngle %= 360; //between 0 and 360
+	//robot.angle += turnAngle;
+	//robot.angle = constrainAngle(robot.angle);
+	return turnAngle;//constrainAngle(robot.angle-turnAngle);
+	//robot.angle %= 360;
 }
 
-const findRobotEndpoints = function() {
-	let leftTop = [(currentRobot.x-(0.5*currentRobot.width)), (currentRobot.y-(0.5*currentRobot.height))];
-	let rightTop = [(currentRobot.x+(0.5*currentRobot.width)), (currentRobot.y-(0.5*currentRobot.height))];
-	let leftBottom = [(currentRobot.x-(0.5*currentRobot.width)), (currentRobot.y+(0.5*currentRobot.height))];
-	let rightBottom = [(currentRobot.x+(0.5*currentRobot.width)), (currentRobot.y+(0.5*currentRobot.height))];
-	robotEndpoints = [leftTop, rightTop, rightBottom, leftBottom, leftTop];
+const constrainAngle = function(angle) {
+	if (angle > 360) {
+		angle -=360;
+	} else if (angle < 0) {
+		angle +=360;
+	}
+	return angle;
+}
+
+const fFromJoystickPower = function(power) {
+	return power*0.001;
+}
+const degFromJoystickPower = function(power) {
+	return power*0.001;
+}
+
+const findRobotEndpoints = function(robot) {
+	let topLAngle = robot.angle-45;
+	let topRAngle = robot.angle+45;
+	let bottomLAngle = robot.angle+180+45;
+	let bottomRAngle = robot.angle+180-45;
+
+	topLAngle = constrainAngle(topLAngle);
+	topRAngle = constrainAngle(topRAngle);
+	bottomLAngle = constrainAngle(bottomLAngle);
+	bottomRAngle = constrainAngle(bottomRAngle);
+
+	let leftTop = [((0.5*robot.width)*Math.cos(topLAngle))+robot.x, ((0.5*robot.height)*Math.sin(topLAngle))+robot.y];
+	let rightTop = [((0.5*robot.width)*Math.cos(topRAngle))+robot.x, ((0.5*robot.height)*Math.sin(topRAngle))+robot.y];
+	let leftBottom = [((0.5*robot.width)*Math.cos(bottomLAngle))+robot.x, ((0.5*robot.height)*Math.sin(bottomLAngle))+robot.y];
+	let rightBottom = [((0.5*robot.width)*Math.cos(bottomRAngle))+robot.x, ((0.5*robot.height)*Math.sin(bottomRAngle))+robot.y];
+	robot.endpoints = [leftTop, rightBottom, leftBottom, [robot.x, robot.y], leftTop];
+	//console.log(robotEndpoints)
 	//return ;
 }
+
+const updateRobot = function(robot, deltaTime, controller) {
+	updateRobotPosition(robot, deltaTime, controller.leftStick.x, controller.leftStick.y, controller.rightStick.x, controller.rightStick.y);
+	findRobotEndpoints(robot);
+}
+
+setInterval( () => {
+	updateRobot(currentRobotA, 1, currentControllerPositions.a);
+	updateRobot(currentRobotB, 1, currentControllerPositions.b);
+}, 10);
+/*setInterval( () => {
+	reduceRobotFinal(currentRobotA);
+	reduceRobotFinal(currentRobotB);
+}, 500);*/
 
 //CONNECT CONTROLLER
 console.log("initting controlers");
 if (!controllerAEnabled && !controllerBEnabled) {
-	throw "no controller was enabled you meme"
+	//throw "no controller was enabled you meme";
 }
 if (controllerAEnabled) {
 	try {
@@ -285,15 +424,38 @@ app.listen(serverPort, () => console.log(`server OK, port=${serverPort}!`));
 
 console.log("websocket bois init");
 
+//WS CODE
+function heartbeat() {
+	console.log("hb from cli");
+	this.isAlive = true;
+}
+function noop() {}
+
 const WebSocket = require('ws')
 const wss = new WebSocket.Server({ port: websocketPort });
 wss.on('connection', ws => {
-  ws.on('message', message => {
-    console.log(`Received message => ${message}`)
-  })
-  //ws.send(JSON.stringify({"status": "OK"}));
-  ws.send(JSON.stringify({"status": "OK", "field": []}));
-  setInterval( () => {
-  	ws.send(JSON.stringify({"status": "OK", "robot": currentRobot, "endpoints": robotEndpoints}));
-  },100);
+	ws.isAlive = true;
+	ws.on('pong', heartbeat);
+
+	ws.on('message', message => {
+	console.log(`Received message => ${message}`)
+	})
+	//ws.send(JSON.stringify({"status": "OK"}));
+	ws.send(JSON.stringify({"status": "OK", "field": []}));
+	setInterval( () => {
+		ws.send(JSON.stringify({"status": "OK", "robotA": currentRobotA, "robotB": currentRobotB}));
+	},100);
 })
+
+
+const WSinterval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) {
+    	console.log("Websocket disconnected (did you close the tab?), terminating");
+    	return ws.terminate();
+    }
+
+    ws.isAlive = false; //set check
+    ws.ping(noop);
+  });
+}, 1000);
--- static/websock.js
+++ static/websock.js
@@ -3,16 +3,31 @@
 console.log("websock loaded");
 
 const WSport = 8080;
-var currentRobotPosition = {
+const fieldWidth = 1000;
+const fieldHeight = 500;
+function heartbeat() {
+  clearTimeout(this.pingTimeout);
+  this.pingTimeout = setTimeout(() => {
+  	console.log("Terminating websocket");
+    this.terminate();
+  }, 1000 + 100); //localhost so latency should be low
+}
+
+var currentRobotPositionA = {
 	x: 0,
 	y: 0
 };
-var robotEndpoints = [[0,0]];
+var currentRobotPositionB = {
+	x: 0,
+	y: 0
+};
+var robotEndpointsA = [[0,0]];
+var robotEndpointsB = [[0,0]];
 var robotField = [];
 
 const field = document.getElementById("field");
-field.width = 500;
-field.height = 500;
+field.width = fieldWidth;
+field.height = fieldHeight;
 const fctx = field.getContext("2d");
 
 ws = new WebSocket(`ws://${location.host}:${WSport}`);
@@ -21,24 +36,33 @@ ws.onerror = (err) => {
 }
 ws.onopen = (msg) => {
 	console.log("open: "+msg);
+	heartbeat();
 	ws.send("hi")
 }
 ws.onclose = (msg) => {
 	console.log("close: "+msg);
+	clearTimeout(this.pingTimeout);
+}
+ws.onping = ping => {
+	heartbeat();
 }
 ws.onmessage = (msg) => {
 	let data = JSON.parse(msg.data);
 	let field = data.field;
 	let status = data.status;
-	let robot = data.robot;
-	let endpoints = data.endpoints;
+	let robotA = data.robotA;
+	let robotB = data.robotB;
 	console.log("msg: ",JSON.parse(msg.data));
-	if (status == "OK" && typeof robot !== "undefined" && typeof endpoints !== "undefined") {
+	if (status == "OK" && typeof robotA !== "undefined" && typeof robotB !== "undefined") {
 		console.log("recieved ok statusupdate");
-		let crp = currentRobotPosition;
-		crp.x = robot.x;
-		crp.y = robot.y;
-		robotEndpoints = endpoints;
+		let crpA = currentRobotPositionA;
+		crpA.x = robotA.x;
+		crpA.y = robotA.y;
+		robotEndpointsA = robotA.endpoints;
+		let crpB = currentRobotPositionB;
+		crpB.x = robotB.x;
+		crpB.y = robotB.y;
+		robotEndpointsB = robotB.endpoints;
 		updateRobotPos();
 	} else if (status == "OK" && typeof field !== "undefined") {
 		console.log("field update");
@@ -50,11 +74,18 @@ ws.onmessage = (msg) => {
 }
 
 function updateRobotPos() {
-	fctx.clearRect(0,0,field.width, field.height);
-	let ep = robotEndpoints;
-	fctx.moveTo(ep[0][0], ep[0][1]);
-	for (var i=1; i<ep.length; i++) {
-		fctx.lineTo(ep[i][0], ep[i][1]);
+	fctx.clearRect(0,0,fieldWidth, fieldHeight);
+	fctx.beginPath();
+	let epA = robotEndpointsA;
+	let epB = robotEndpointsB;
+	fctx.moveTo(epA[0][0], epA[0][1]);
+	for (var i=1; i<epA.length; i++) {
+		fctx.lineTo(epA[i][0], epA[i][1]);
+	}
+	fctx.moveTo(epB[0][0], epB[0][1]);
+	for (var i=1; i<epB.length; i++) {
+		fctx.lineTo(epB[i][0], epB[i][1]);
 	}
 	fctx.stroke();
+
 }
